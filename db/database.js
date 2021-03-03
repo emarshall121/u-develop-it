48dc0a2783a679ea1d99042bdb2626372c518476
EMarshall <emilyjmarshall121@gmail.com>
2021-03-02 20:15:44 -0700

    voterRoutes

---
+++ db/database.js
@@ -0,0 +1,13 @@
+// SQL database - .verbose produces messages in terminal about the state of the runtime
+const sqlite3 = require('sqlite3').verbose();
+
+// Connect to database
+const db = new sqlite3.Database('./db/election.db', err => {
+  if (err) {
+    return console.error(err.message);
+  }
+
+  console.log('Connected to the election database.');
+});
+
+module.exports = db;
